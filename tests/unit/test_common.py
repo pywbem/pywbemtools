5a464a1732bd50f567a5e509673094072b3f9fcc
kschopmeyer <k.schopmeyer@swbell.net>
2018-09-10 19:56:15 -0500

    Fix version of pytest-cov to match pywbem and eliminate failure in test.
    Also fixes a couple of places pywbem name used and should be pywbemtools.
    
    Remove test prints. These were part of temp  tests
    
    fix. Not sure why but now reporting error 6 and not 5
    
    Used opportunity to fix test_class_subcmd.py tests

--- tests/unit/test_common.py
+++ tests/unit/test_common.py
@@ -131,18 +131,7 @@ class TestParseWbemUri(object):
                 namespace=u'root/cimv2',
                 keys={'k1': 'v1'},
                 host=u'10.11.12.13:5989'),
-            None, True
-        ),
-        (
-            "class and keybinding only",
-            'CIM_Foo.k1="v1"',
-            None,
-            dict(
-                classname=u'CIM_Foo',
-                namespace=None,
-                keys={'k1': 'v1'},
-                host=None,),
-            None, True
+            None, False
         ),
     ]
 
@@ -193,7 +182,6 @@ class TestParseWbemUri(object):
             else:
 
                 # The code to be tested
-                print('BEFORE uri=%s' % uri)
                 obj = parse_wbemuri_str(uri)
 
         if exp_attrs:
