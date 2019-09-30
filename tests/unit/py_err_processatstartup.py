8c43cf2888cb206c2afdf3bbab645997cf8f6d92
kschopmeyer <k.schopmeyer@swbell.net>
2019-09-30 09:13:29 -0500

    Fixes issue #419. Mixed old an new formatters
    One ClickException statement mixed the old and new formatters using the
    {} from the new but the % template/variable list separator from the new
    formatter.
    
    Includes documentation in docs/changes.rst

---
+++ tests/unit/py_err_processatstartup.py
@@ -0,0 +1,14 @@
+# !PROCESS!AT!STARTUP!
+"""
+This is an error test of the code that processes a script at startup. This
+python code specifically includes a syntax error to be caught by pywbemcli.
+Note that this file is marked to process at startup which is really a private
+extension for the test environment.
+"""
+
+
+def mock?prompt(msg):  # noqa: E999
+    """Mock function to replace pywbemcli_prompt and return a value"""
+    # pylint: disable=undefined-variable
+    print(msg)
+    return "HaHa"
