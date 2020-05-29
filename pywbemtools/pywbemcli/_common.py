a823fa5518d9baf1f4596c78bd866b5ae6c2b237
Andreas Maier <andreas.r.maier@gmx.de>
2020-05-29 19:07:39 +0200

    Fixed flake8 issues with whitespace
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- pywbemtools/pywbemcli/_common.py
+++ pywbemtools/pywbemcli/_common.py
@@ -416,7 +416,7 @@ def filter_namelist(pattern, name_list, ignore_case=True):
         raise click.ClickException('Regex compile error. Regex={}. Er: {}: {}'
                                    .format(regex, ex.__class__.__name__, ex))
 
-    new_list = [n for n in name_list for m in[compiled_regex.match(n)] if m]
+    new_list = [n for n in name_list for m in [compiled_regex.match(n)] if m]
 
     return new_list
 
