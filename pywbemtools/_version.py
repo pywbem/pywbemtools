1cdbc6730a9e8553cb99e400a18b89ff93415b3a
Andreas Maier <andreas.r.maier@gmx.de>
2020-07-31 09:57:01 +0200

    Release 0.7.1
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- pywbemtools/_version.py
+++ pywbemtools/_version.py
@@ -25,4 +25,4 @@ Version of the pywbemtools package.
 #:
 #: * "M.N.P.devD": Development level D of a not yet released version M.N.P
 #: * "M.N.P": A released version M.N.P
-__version__ = '0.7.1.dev1'
+__version__ = '0.7.1'
