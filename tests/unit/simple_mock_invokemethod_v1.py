fced6b9e59dcc903155cece78dfb09628d30ad3b
Andreas Maier <andreas.r.maier@gmx.de>
2020-08-08 08:41:44 +0200

    Test: Fixed method invocations
    Details:
    
    * The updated provider dispatcher in the mock support of pywbem 1.0.0
      now verifies that non-static methods are not called on class objects.
      However, several test cases in test_class_cmds.py did attempt to invoke
      the non-static Fuzzy() method on the CIM_Foo class object.
    
      Fixed that by adding a static method FuzzyStatic() to the MOF and by
      updating test cases as needed.
    
    * Fixed incompatible parameters for MethodProvider.InvokeMethod() in the
      test providers all_types_method_mock_v1.py and simple_mock_invokemethod_v1.py
      that had changed in pywbem_mock meanwhile (during the course of developing
      pywbem 1.0.0).
    
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- tests/unit/simple_mock_invokemethod_v1.py
+++ tests/unit/simple_mock_invokemethod_v1.py
@@ -1,10 +1,8 @@
 """
 mock_pywbem test script that installs a a method provider for the class
 CIM_Foo
-
 """
 
-import six
 from pywbem_mock import MethodProvider
 
 from pywbem import CIM_ERR_METHOD_NOT_AVAILABLE, CIMError, CIM_ERR_NOT_FOUND, \
@@ -32,22 +30,17 @@ class CIM_FooMethodProvider(MethodProvider):
     def __init__(self, cimrepository):
         super(CIM_FooMethodProvider, self).__init__(cimrepository)
 
-    def InvokeMethod(self, namespace, MethodName, ObjectName, Params):
+    def InvokeMethod(self, methodname, localobject, params):
         """
-        Simplistic test method. Validates methodname, objectname, Params
+        Simplistic test method. Validates methodname, localobject, params
         and returns rtn value 0 and one parameter
 
         The parameters and return for Invoke method are defined in
         :meth:`~pywbem_mock.MethodProvider.InvokeMethod`
         """
-        # validate namespace using method in BaseProvider
-        self.validate_namespace(namespace)
+        namespace = localobject.namespace
+        classname = localobject.classname
 
-        # get classname and validate. This provider uses only one class
-        if isinstance(ObjectName, six.string_types):
-            classname = ObjectName
-        else:
-            classname = ObjectName.classname
         assert classname.lower() == 'cim_foo'
 
         # Test if class exists.
@@ -57,25 +50,25 @@ class CIM_FooMethodProvider(MethodProvider):
                 "class {0} does not exist in CIM repository, "
                 "namespace {1}".format(classname, namespace))
 
-        if isinstance(ObjectName, CIMInstanceName):
+        if isinstance(localobject, CIMInstanceName):
             instance_store = self.cimrepository.get_instance_store(namespace)
-            if not instance_store.object_exists(ObjectName):
+            if not instance_store.object_exists(localobject):
                 raise CIMError(
                     CIM_ERR_NOT_FOUND,
                     "Instance {0} does not exist in CIM repository",
-                    format(ObjectName))
+                    format(localobject))
 
         # This method expects a single parameter input
         return_params = []
-        if MethodName.lower() == 'fuzzy':
-            if Params:
-                if 'TestInOutParameter' in Params:
-                    return_params.append(Params['TestInOutParameter'])
+        if methodname.lower() in ('fuzzy', 'fuzzystatic'):
+            if params:
+                if 'TestInOutParameter' in params:
+                    return_params.append(params['TestInOutParameter'])
 
-            if 'TestRef' in Params:
-                return_params.append(Params['TestRef'])
+            if 'TestRef' in params:
+                return_params.append(params['TestRef'])
 
-            return_value = Params.get('OutputRtnValue', 0)
+            return_value = params.get('OutputRtnValue', 0)
 
             return (return_value, return_params)
 
