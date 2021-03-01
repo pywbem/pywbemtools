fb89d47cfd8f9bcc96b2cde9f5f291f86341133e
Andreas Maier <andreas.r.maier@gmx.de>
2021-03-01 11:23:32 +0100

    Test: Support for simple interop mock script for py27
    Details:
    
    * So far, the simple interop mock script simple_interop_mock_script.py supported
      only Python 3.5 and above because it used the new-style setup approach.
      This change enhances that mock script to add support for old-style
      setup when being used on Python below 3.5.
    
    * Removed the conditional on the namespace testcases that use this mock
      script so that they are now run on all python versions.
    
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- tests/unit/simple_interop_mock_script.py
+++ tests/unit/simple_interop_mock_script.py
@@ -2,8 +2,14 @@
 Test mock script that installs the pywbem provided namespace provider
 CIMNamespaceProvider and the simple Interop and user namespace model
 defined in simple_interop_mock_model.mof.
+
+This mock script uses 'new-style' setup for Python >=3.5 and 'old-style' setup
+otherwise, and is therefore useable for all supported Python versions.
+On Python <3.5, the tests using this script must be run from the repo main
+directory.
 """
 
+import sys
 import os
 import pywbem_mock
 
@@ -11,7 +17,7 @@ import pywbem_mock
 INTEROP_NAMESPACE = 'interop'
 
 
-def setup(conn, server, verbose):
+def _setup(conn, server, verbose):
     # pylint: disable=unused-argument
     """
     Setup for this mock script.
@@ -25,17 +31,52 @@ def setup(conn, server, verbose):
     if INTEROP_NAMESPACE not in conn.cimrepository.namespaces:
         conn.add_namespace(INTEROP_NAMESPACE)
 
-    # This MOF file sets pragma namespace to 'interop' and 'root/cimv2'
-    mof_file = os.path.join(os.path.dirname(__file__),
+    if sys.version_info >= (3, 5):
+        this_file_path = __file__
+    else:
+        # Unfortunately, it does not seem to be possible to find the file path
+        # of the current script when it is executed using exec(), so we hard
+        # code the file path. This requires that the tests are run from the
+        # repo main directory.
+        this_file_path = 'tests/unit/simple_interop_mock_script.py'
+        assert os.path.exists(this_file_path)
+
+    mof_file = os.path.join(os.path.dirname(this_file_path),
                             'simple_interop_mock_model.mof')
-    mof_inc1_file = os.path.join(os.path.dirname(__file__),
+    mof_inc1_file = os.path.join(os.path.dirname(this_file_path),
                                  'mock_interop.mof')
-    mof_inc2_file = os.path.join(os.path.dirname(__file__),
+    mof_inc2_file = os.path.join(os.path.dirname(this_file_path),
                                  'simple_mock_model.mof')
     conn.compile_mof_file(mof_file, namespace=None)
-    conn.provider_dependent_registry.add_dependents(__file__, mof_file)
-    conn.provider_dependent_registry.add_dependents(__file__, mof_inc1_file)
-    conn.provider_dependent_registry.add_dependents(__file__, mof_inc2_file)
+    conn.provider_dependent_registry.add_dependents(
+        this_file_path, mof_file)
+    conn.provider_dependent_registry.add_dependents(
+        this_file_path, mof_inc1_file)
+    conn.provider_dependent_registry.add_dependents(
+        this_file_path, mof_inc2_file)
 
     ns_provider = pywbem_mock.CIMNamespaceProvider(conn.cimrepository)
     conn.register_provider(ns_provider, INTEROP_NAMESPACE, verbose=verbose)
+
+
+if sys.version_info >= (3, 5):
+    # New-style setup
+
+    # If the function is defined directly, it will be detected and refused
+    # by the check for setup() functions on Python <3.5, despite being defined
+    # only conditionally. The indirect approach with exec() addresses that.
+    # pylint: disable=exec-used
+    exec("""
+def setup(conn, server, verbose):
+    _setup(conn, server, verbose)
+""")
+
+else:
+    # Old-style setup
+
+    global CONN  # pylint: disable=global-at-module-level
+    global SERVER  # pylint: disable=global-at-module-level
+    global VERBOSE  # pylint: disable=global-at-module-level
+
+    # pylint: disable=undefined-variable
+    _setup(CONN, SERVER, VERBOSE)  # noqa: F821
