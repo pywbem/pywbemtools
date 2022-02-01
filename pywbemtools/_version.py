6a853f7135206de993ac558ba1996ecd9137c171
Andreas Maier <andreas.r.maier@gmx.de>
2022-02-01 18:02:36 +0100

    Start 1.0.1
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- pywbemtools/_version.py
+++ pywbemtools/_version.py
@@ -25,4 +25,4 @@ Version of the pywbemtools package.
 #:
 #: * "M.N.P.devD": Development level D of a not yet released version M.N.P
 #: * "M.N.P": A released version M.N.P
-__version__ = '1.0.0'
+__version__ = '1.0.1.dev1'
