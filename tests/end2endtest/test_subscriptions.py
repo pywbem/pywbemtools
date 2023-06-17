91c1ca83c2298a4b57ba4d5c87ea921bcc1404a9
kschopmeyer <k.schopmeyerwork@gmail.com>
2023-06-17 15:03:56 -0500

    Add Indication end2end test module.
    Added new module test_indications to tests/end2endtests. This
    module creates a subscription and a listener and requests that the
    OpenPegasus server send a defined number of indications to the
    listener.
    
    This tests the capabilities for a container based WBEM server
    to send indications to a pywbemlistener.
    
    Created common utility functions for things that are used in multiple
    commands primarily validing existence in the server and creating
    subscriptions.  Put these into the end2end utils.py
    
    Moved some utility functions from end2endtest tests (ex.
    indication subscription creation) to utils.py to simplify the test code. Created
    utility function to execute pywbemcli cmdline.
    
    This test depends one existence of OpenPegasus Docker 0.1.3.
    
    NOTE: Modified test.yaml because I had not been keeping the
    openpegasus version the same in the Makefile and test.yaml.  Version
    0.1.3 is the current required version to pass the test_indications test.
    Note that both changes are in the context of the current release.
    
    Modified the end2end utils.py to allow separate container control by
    setting a variable. This makes testing new end2end tests easier in that
    the container does not stop at the end of the test and the container can
    also start OpenPegasus with modified parameters to aid testing
    (i.e. trace enabled.)

--- tests/end2endtest/test_subscriptions.py
+++ tests/end2endtest/test_subscriptions.py
@@ -23,9 +23,11 @@ from __future__ import absolute_import, print_function
 import re
 
 # pylint: disable=unused-import
-from .utils import server_url  # noqa: F401
+from .utils import server_url, validate_namespace_exists, \
+    validate_indication_profile, validate_required_classes, \
+    create_indication_subscription, remove_subscription, \
+    exec_pywbemcli_cmd  # noqa: F401
 # pylint: enable=unused-import
-from ..unit.utils import execute_command
 
 
 def test_subscriptions(server_url):
@@ -34,138 +36,33 @@ def test_subscriptions(server_url):
     The test.  This function is called because server_url is a pytest
     fixture.
     """
-    # Get Interop Namespace
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
-        ['-s', server_url, '--no-verify', 'namespace', 'interop'])
-    assert rc == 0
-    assert stderr == ''
-    interop = stdout.strip('\n')
-    assert interop == 'root/interop'
+    interop = validate_namespace_exists(server_url, 'interop', 'root/interop')
 
-    # Determine if Indication profile exists
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
-        ['-s', server_url, '--no-verify', 'profile', 'list'])
-    assert rc == 0
-    assert stderr == ''
-    profile_lines = stdout.strip('\n').split('\n')[3:]
-    profiles = []
-    for line in profile_lines:
-        m = re.match(r'^(.+?) {2,}(.+?) {2,}(.+)$', line)
-        assert m, "Cannot parse 'profile list' output line: {!r}".format(line)
-        profiles.append(m.groups())
-    # validate versions of indication profile
-    assert ('SNIA', 'Indication', '1.2.0') in profiles
+    validate_indication_profile(server_url, ('SNIA', 'Indication', '1.2.0'))
 
-    # Determine if required classes are in interop
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
-        ['-s', server_url, '--no-verify', 'class', 'get',
-         'CIM_ListenerDestinationCIMXML', '-n', interop])
-    assert rc == 0
-    assert stderr == ''
+    required_classes = {interop: ['CIM_ListenerDestinationCIMXML',
+                                  'CIM_IndicationFilter',
+                                  'CIM_IndicationSubscription', ], }
 
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
-        ['-s', server_url, '--no-verify', 'class', 'get',
-         'CIM_IndicationFilter', '-n', interop])
-    assert rc == 0
-    assert stderr == ''
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
-        ['-s', server_url, '--no-verify', 'class', 'get',
-         'CIM_IndicationSubscription', '-n', interop])
-    assert rc == 0
-    assert stderr == ''
+    validate_required_classes(server_url, required_classes)
 
-    # Create a destination
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
-        ['-s', server_url, '--no-verify', 'subscription', 'add-destination',
-         'odest1', '-l', 'http://localhost:5000'])
-    assert rc == 0
-    assert stderr == ''
-    good_response = stdout.strip('\n')
-    assert good_response == 'Added owned destination: ' \
-        'Name=pywbemdestination:defaultpywbemcliSubMgr:odest1'
+    filter_id = 'ofilter1'
+    dest_id = 'odest1'
 
-    # Create a filter
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
-        ['-s', server_url, '--no-verify', 'subscription', 'add-filter',
-         'ofilter1', '--query', 'Select * from CIM_AlertIndication'])
-    assert rc == 0
-    assert stderr == ''
-    good_response = stdout.strip('\n')
-    assert good_response == 'Added owned filter: Name=pywbemfilter:' \
-        'defaultpywbemcliSubMgr:ofilter1'
-
-    # Test filter creation that returns CIMError since we cannot test
-    # this with mock. Server should return exception because of bad filter
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
-        ['-s', server_url, '--no-verify', 'subscription', 'add-filter',
-         'ofilter2', '--query', 'blah'])
-    assert rc == 1
-    err_response = stderr.strip('\n')
-    assert re.search("CIM_ERR_INVALID_PARAMETER", err_response)
-
-    # Create a subscription
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
-        ['-s', server_url, '--no-verify', 'subscription', 'add-subscription',
-         'odest1', 'ofilter1', '--owned'])
-    assert rc == 0
-    assert stderr == ''
-    good_response = stdout.strip('\n')
-    assert re.search("Added owned subscription:", good_response)
-    assert re.search('pywbemdestination:defaultpywbemcliSubMgr:odest1',
-                     good_response)
+    create_indication_subscription(server_url, 'http', 'localhost', '5000',
+                                   dest_id, filter_id,
+                                   'Select * from CIM_AlertIndication',
+                                   'root/cimv2')
 
     # General list command
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
+    stdout = exec_pywbemcli_cmd(
         ['-s', server_url, '--no-verify', '-o', 'simple',
          'subscription', 'list'])
-    assert rc == 0
-    assert stderr == ''
+
     good_response = stdout.strip('\n')
     pattern = r"TOTAL INSTANCES +3 +0 +3"
     assert re.search(pattern, good_response)
 
     # Remove the subscription
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
-        ['-s', server_url, '--no-verify', 'subscription', 'remove-subscription',
-         'odest1', 'ofilter1'])
-    assert rc == 0
-    assert stderr == ''
-    good_response = stdout.strip('\n')
-    assert good_response == \
-        'Removed 1 subscription(s) for destination-id: odest1, filter-id: ' \
-        'ofilter1.'
 
-    # Remove the filter
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
-        ['-s', server_url, '--no-verify', 'subscription', 'remove-filter',
-         'ofilter1'])
-    assert rc == 0
-    assert stderr == ''
-    good_response = stdout.strip('\n')
-    assert good_response == \
-        'Removed owned indication filter: identity=ofilter1, ' \
-        'Name=pywbemfilter:defaultpywbemcliSubMgr:ofilter1.'
-
-    # Remove the destination
-    rc, stdout, stderr = execute_command(
-        'pywbemcli',
-        ['-s', server_url, '--no-verify', 'subscription', 'remove-destination',
-         'odest1'])
-    assert rc == 0
-    assert stderr == ''
-    good_response = stdout.strip('\n')
-    assert good_response == \
-        'Removed owned indication destination: identity=odest1, ' \
-        'Name=pywbemdestination:defaultpywbemcliSubMgr:odest1.'
+    remove_subscription(server_url, dest_id, filter_id)
